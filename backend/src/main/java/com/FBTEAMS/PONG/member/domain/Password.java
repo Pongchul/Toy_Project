443513fccef025b952d37b55dbc43cb0703eaa29
pongchul <inchul7511@gmail.com>
2023-09-19 20:27:47 +0900

    Refactoring: Advise Code

--- backend/src/main/java/com/FBTEAMS/PONG/member/domain/Password.java
+++ backend/src/main/java/com/FBTEAMS/PONG/member/domain/Password.java
@@ -4,4 +4,7 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Password {
+
+    private final int MIN_LENGTH = 8;
+    private final int MAX_LENGTH = 30;
 }
--- backend/src/main/java/com/FBTEAMS/PONG/member/domain/UserName.java
+++ backend/src/main/java/com/FBTEAMS/PONG/member/domain/UserName.java
@@ -4,4 +4,7 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class UserName {
+
+    private final int MIN_LENGTH = 1;
+    private final int MAX_LENGTH = 20;
 }
