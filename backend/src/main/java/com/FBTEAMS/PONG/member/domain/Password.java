91052b03fd17d6e53eda92db6865748393f7b559
pongchul <inchul7511@gmail.com>
2023-09-20 20:53:53 +0900

    Add formatting

--- backend/src/main/java/com/FBTEAMS/PONG/member/domain/Password.java
+++ backend/src/main/java/com/FBTEAMS/PONG/member/domain/Password.java
@@ -1,10 +1,23 @@
 package com.FBTEAMS.PONG.member.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor
 public class Password {
 
-    private final int MIN_LENGTH = 8;
-    private final int MAX_LENGTH = 30;
+    private static final int MIN_LENGTH = 8;
+    private static final int MAX_LENGTH = 30;
+    private static final String PASSWORD_FORMAT = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$";
+
+    @Column(name = "password")
+    private String value;
+
+    public Password(String value) {
+        this.value = value;
+    }
+
+
 }
